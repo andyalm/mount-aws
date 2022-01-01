afd08c14cd1f38cdf71d11c8ccde17e525da1fbe
Andy Alm <andy.alm@gettyimages.com>
2021-12-31 18:16:42 -0800

    Minor code polishing

--- MountAws.Impl/LinqExtensions.cs
+++ MountAws.Impl/LinqExtensions.cs
@@ -1,16 +1,14 @@
-using System.Management.Automation;
 using MountAnything;
 
 namespace MountAws;
 
 internal static class LinqExtensions
 {
-    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+    public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
     {
         foreach (var item in enumerable)
         {
             action(item);
-            yield return item;
         }
     }
 
--- MountAws.Impl/MountAwsRouterFactory.cs
+++ MountAws.Impl/MountAwsRouterFactory.cs
@@ -11,29 +11,23 @@ public class MountAwsRouterFactory : IRouterFactory
         var router = Router.Create<ProfilesHandler>();
         router.RegisterServices(builder =>
         {
-            var registrars = typeof(CoreServiceRegistrar).Assembly
+            typeof(CoreServiceRegistrar).Assembly
                 .GetTypes()
                 .Where(t => typeof(IServiceRegistrar).IsAssignableFrom(t) && !t.IsAbstract)
                 .Select(Activator.CreateInstance)
-                .Cast<IServiceRegistrar>();
-            foreach (var registrar in registrars)
-            {
-                registrar.Register(builder);
-            }
+                .Cast<IServiceRegistrar>()
+                .ForEach(registrar => registrar.Register(builder));
         });
         router.Map<ProfileHandler, CurrentProfile>(profile =>
         {
             profile.Map<RegionHandler, CurrentRegion>( region =>
             {
-                var serviceRoutes = typeof(IServiceRoutes).Assembly
+                typeof(IServiceRoutes).Assembly
                     .GetTypes()
                     .Where(t => typeof(IServiceRoutes).IsAssignableFrom(t) && !t.IsAbstract)
                     .Select(Activator.CreateInstance)
-                    .Cast<IServiceRoutes>();
-                foreach (var serviceRouter in serviceRoutes)
-                {
-                    serviceRouter.AddServiceRoutes(region);
-                }
+                    .Cast<IServiceRoutes>()
+                    .ForEach(serviceRouter => serviceRouter.AddServiceRoutes(region));
             });
         });
 
