39cbd912b0b004ba961fceed0f69fb46f8ff33b2
Andy Alm <andy.alm@gettyimages.com>
2022-01-01 11:12:41 -0800

    Further shrink the size of the module by eliminating more unnecessary assemblies

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,5 @@
 $ErrorActionPreference='Stop'
 
 dotnet publish MountAws.Host
+Get-ChildItem -Recurse ./bin/MountAws | Select-Object Name
 Publish-Module -Path ./bin/MountAws -NuGetApiKey $env:NuGetApiKey
