a3b0d772d9270240047b9db1ea815ec1ecd6f1ba
Andy Alm <andy.alm@gettyimages.com>
2022-11-14 20:26:19 -0800

    Fixed possible NullReferenceException when listing elasticache replication groups.

--- MountAws/Services/Elasticache/ModelExtensions.cs
+++ MountAws/Services/Elasticache/ModelExtensions.cs
@@ -4,8 +4,13 @@ namespace MountAws.Services.Elasticache;
 
 public static class ModelExtensions
 {
-    public static string ToAddressAndPortString(this Endpoint endpoint)
+    public static string? ToAddressAndPortString(this Endpoint? endpoint)
     {
+        if (endpoint == null)
+        {
+            return null;
+        }
+        
         return $"{endpoint.Address}:{endpoint.Port}";
     }
 }
