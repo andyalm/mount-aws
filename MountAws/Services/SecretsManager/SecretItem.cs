3e5e80021b720320c4564c71b42ffe34432890b1
Andy Alm <andy.alm@gettyimages.com>
2026-02-28 21:39:03 -0800

    Simplified the SecretItem

--- MountAws/Services/SecretsManager/SecretItem.cs
+++ MountAws/Services/SecretsManager/SecretItem.cs
@@ -4,46 +4,19 @@ using MountAnything;
 
 namespace MountAws.Services.SecretsManager;
 
-public class SecretItem : AwsItem
+public class SecretItem(ItemPath parentPath, SecretListEntry secret) : AwsItem(parentPath, new PSObject(secret))
 {
-    public SecretItem(ItemPath parentPath, SecretListEntry secret) : base(parentPath, new PSObject(secret))
-    {
-        ItemName = new ItemPath(secret.Name).Name;
-        SecretName = secret.Name;
-        ItemType = SecretsManagerItemTypes.Secret;
-        Description = secret.Description;
-        Arn = secret.ARN;
-        LastChangedDate = secret.LastChangedDate;
-        CreatedDate = secret.CreatedDate;
-    }
-
-    public SecretItem(ItemPath parentPath, DescribeSecretResponse secret) : base(parentPath, new PSObject(secret))
-    {
-        ItemName = new ItemPath(secret.Name).Name;
-        SecretName = secret.Name;
-        ItemType = SecretsManagerItemTypes.Secret;
-        Description = secret.Description;
-        Arn = secret.ARN;
-        LastChangedDate = secret.LastChangedDate;
-        CreatedDate = secret.CreatedDate;
-    }
-
-    public override string ItemName { get; }
+    public override string ItemName { get; } = new ItemPath(secret.Name).Name;
     public override bool IsContainer => false;
     protected override string TypeName => GetType().FullName!;
-    public override string ItemType { get; }
+    public override string ItemType => SecretsManagerItemTypes.Secret;
+
     [ItemProperty]
     public string Name => ItemName;
     [ItemProperty]
-    public string SecretName { get; }
+    public string SecretName => secret.Name;
     [ItemProperty]
-    public string? Description { get; }
-    [ItemProperty]
-    public string Arn { get; }
-    [ItemProperty]
-    public DateTime? LastChangedDate { get; }
-    [ItemProperty]
-    public DateTime? CreatedDate { get; }
+    public string Arn => secret.ARN;
 
     public override string? WebUrl =>
         UrlBuilder.CombineWith($"secretsmanager/secret?name={Uri.EscapeDataString(SecretName)}");
